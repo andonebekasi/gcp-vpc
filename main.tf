c01fd659c24c6b38ee9bc770126bbaebe48eed2f
andi <34510976+andonebekasi@users.noreply.github.com>
2021-11-04 15:19:09 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,14 +3,10 @@ provider "google" {
   region  = "asia-southeast2"
 }
 
-resource "google_compute_network" "vpc_network-1" {
+resource "google_compute_network" "vpc_network" {
   name = "VPC-Danamon-1"
   ip_cidr_range = "192.168.1.0/24"
   region        = "asia-southeast2"
 }
 
-resource "google_compute_network" "vpc_network-2" {
-  name = "VPC-Danamon-2"
-  ip_cidr_range = "192.168.2.0/24"
-  region        = "asia-southeast2"
-}
+
