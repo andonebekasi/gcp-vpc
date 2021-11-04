0eba965e1f703c978baa9eafa54bf61ffabd1052
andi <34510976+andonebekasi@users.noreply.github.com>
2021-11-04 14:05:55 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,8 @@
-provider “google” {
-
-project = “poc-danamon-devsecops”
-
-region = “asia-southeast2”
-
+provider "google" {
+  project = "poc-danamon-devsecops"
+  region  = "asia-southeast2"
 }
 
 resource “google-compute_network” “vpc_network” {
-
 name = “test_terraform_vpc”
-
 }
