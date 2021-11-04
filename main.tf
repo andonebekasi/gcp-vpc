a266236d1b149f13ef0aaee2ad3c610c45449574
andi <34510976+andonebekasi@users.noreply.github.com>
2021-11-04 19:18:25 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,4 @@ resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" 
   ip_cidr_range = "192.168.0.0/16"
   region        = "asia-southeast2"
   network       = google_compute_network.custom-test.id
-  secondary_ip_range {
- }
 }
