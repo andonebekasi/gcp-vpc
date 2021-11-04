910cab13730a29c80a537fa20c698609d00312de
andi <34510976+andonebekasi@users.noreply.github.com>
2021-11-04 15:16:40 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,30 +4,13 @@ provider "google" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  name = "vpc-network"
+  name = "VPC-Danamon-1"
+  ip_cidr_range = "192.168.1.0/24"
+  region        = "asia-southeast2"
 }
 
-subnets = [
-    {
-      subnet_name   = "subnet-01"
-      subnet_ip     = "10.10.10.0/24"
-      subnet_region = "us-west1"
-    },
-    {
-      subnet_name           = "subnet-02"
-      subnet_ip             = "10.10.20.0/24"
-      subnet_region         = "us-west1"
-      subnet_private_access = "true"
-      subnet_flow_logs      = "true"
-    },
-    {
-      subnet_name               = "subnet-03"
-      subnet_ip                 = "10.10.30.0/24"
-      subnet_region             = "us-west1"
-      subnet_flow_logs          = "true"
-      subnet_flow_logs_interval = "INTERVAL_10_MIN"
-      subnet_flow_logs_sampling = 0.7
-      subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
-    }
-  ]
+resource "google_compute_network" "vpc_network" {
+  name = "VPC-Danamon-2"
+  ip_cidr_range = "192.168.2.0/24"
+  region        = "asia-southeast2"
 }
