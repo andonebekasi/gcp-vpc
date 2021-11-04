404f4b8eb8719973bfddeb426c714e014d7c054b
andi <34510976+andonebekasi@users.noreply.github.com>
2021-11-04 15:52:15 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ provider "google" {
 resource "google_compute_network" "custom-test" {
   name = "VPC-Danamon"
   ip_cidr_range = "10.2.0.0/16"
-  region        = "asia-southeast2"
   network       = google_compute_network.custom-test.id
 }
 
